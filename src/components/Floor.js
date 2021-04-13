b9a9cd141682624c2c7c38090f38a35dfd50e2b8
Thomas <kindermann@kitconcept.com>
2021-04-13 14:47:26 +0200

    changed server location

--- src/components/Floor.js
+++ src/components/Floor.js
@@ -4,8 +4,8 @@ import io from "socket.io-client";
 import "../App.css";
 
 // location of server
-//const ENDPOINT = "https://avatario-test.herokuapp.com/";
-const ENDPOINT = "localhost:5000";
+const ENDPOINT = "https://avatario-test.herokuapp.com/";
+//const ENDPOINT = "localhost:5000";
 
 //empty socket for connection
 let socket;
